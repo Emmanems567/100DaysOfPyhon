4a7b5f4b8d3420aba2fab12a9deeebdb6e190830
Emmanems567 <emmanuelalejandro.adame@gmail.com>
2024-01-24 01:02:21 -0600

    Create project 3 code (Day 3 project)

---
+++ Day3/main.py
@@ -0,0 +1,79 @@
+# number = int(input())
+
+# operation = number % 2
+
+# if (operation == 0):
+#     print("This is an even number")
+# else:
+#     print("This is an odd number")
+#=========================
+
+# height = float(input())
+# weight = int(input())
+
+# bmi = (weight / (height ** 2))
+
+# if (bmi < 18.5):
+#     print(f"Your BMI is: {bmi}. You're underweight.")
+# elif (bmi >= 18.5 and bmi < 25):
+#     print(f"Your BMI is: {bmi}. You're at normal weight.")
+# elif (bmi >= 25 and bmi < 30):
+#     print(f"Your BMI is: {bmi}. You're slightly overweight.")
+# elif (bmi >= 30 and bmi < 35):
+#     print(f"Your BMI is : {bmi}. You're obese.")
+# elif (bmi >= 35):
+#     print(f"Your BMI is: {bmi}. You're clinically obese.")
+
+#=========================
+
+# year = int(input())
+
+# if (year % 4 == 0):
+#     if (year % 100 == 0):
+#         if (year % 400 == 0):
+#             print("Leap year")
+#         else:
+#             print("Not leap year")     
+#     else:
+#         print("Not leap year")
+# else:
+#     print("Not leap year")
+
+#=========================
+
+# print("Thank you for choosing Python Pizza Deliveries!")
+# size = input("What size of pizza do you want? [S][M]][L] ")
+# pepperoni = input("Do you want pepperoni? [Y][N] ")
+# cheese = input("Do you want extra cheese? [Y[N] ")
+
+# bill = 0
+
+# if (size == "S"):
+
+#     bill += 15
+        
+# elif (size == "M"):
+    
+#     bill += 20
+
+# elif (size == "L"):
+
+#     bill += 25
+
+# if (size == "S" and pepperoni == "Y"):
+        
+#         bill += 2
+
+# if (size == "M" or size == "L"):
+
+#     if (pepperoni == "Y"):
+#         bill += 3
+
+# if (cheese == "Y"):
+#     bill += 1
+
+# print(f"Your final bill is ${bill}")
+
+#=========================
+
+
